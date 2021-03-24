c83a37d41b6edeff9edfcf858be83e63d10e443b
Gordon Lin <contact@gordonlin.ca>
2021-03-24 00:22:45 -0400

    Included game key

--- src/helpers/games.ts
+++ src/helpers/games.ts
@@ -1,4 +1,5 @@
 import Board from './board';
+import { v4 as uuidv4 } from 'uuid';
 
 enum GameConst{
     MAX_PLAYERS = 4,
@@ -17,10 +18,12 @@ export default class Games{
     private users : Array<gamePlayer>;
     private board : Board;
     private gameStarted : boolean;
+    private gameKey : string;
     constructor(){
         this.users = new Array();
         this.board = new Board();
         this.gameStarted = false;
+        this.gameKey = uuidv4().slice(-5);
     }
     startGame() : boolean{
         if(this.users.length < GameConst.MAX_PLAYERS) return false;
