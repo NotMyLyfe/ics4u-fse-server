4b4be5cd93686318e28aa42690700953228d52ef
Gordon Lin <contact@gordonlin.ca>
2021-03-24 00:22:25 -0400

    Completed basic board

--- src/helpers/board.ts
+++ src/helpers/board.ts
@@ -31,24 +31,30 @@ interface AdjacentNodes{
 }
 
 const HARBOUR_LOC = [
-    [NODE.NORTH, NODE.NORTH_WEST], [NODE.NORTH, NODE.NORTH_EAST], [-1, -1],
-    [NODE.NORTH_WEST, NODE.SOUTH_WEST], [-1, -1], [-1, -1], [NODE.NORTH, NODE.NORTH_EAST],
-    [-1, -1], [-1, -1], [-1, -1], [-1, -1], [NODE.NORTH_EAST, NODE.SOUTH_EAST],
-    [NODE.NORTH_WEST, NODE.SOUTH_WEST], [-1, -1], [-1, -1], [NODE.SOUTH_EAST, NODE.SOUTH],
-    [NODE.SOUTH_WEST, NODE.SOUTH], [NODE.SOUTH_EAST, NODE.SOUTH], [-1, -1] 
-];
+    [0, 0, NODE.NORTH, NODE.NORTH_WEST],
+    [0, 1, NODE.NORTH, NODE.NORTH_EAST],
+    [1, 0, NODE.NORTH_WEST, NODE.SOUTH_WEST],
+    [1, 3, NODE.NORTH, NODE.NORTH_EAST],
+    [2, 4, NODE.NORTH_EAST, NODE.SOUTH_EAST],
+    [3, 0, NODE.NORTH_WEST, NODE.SOUTH_WEST],
+    [3, 3, NODE.SOUTH, NODE.SOUTH_EAST],
+    [4, 0, NODE.SOUTH, NODE.SOUTH_WEST],
+    [4, 1, NODE.SOUTH, NODE.SOUTH_EAST]
+]
 
 class Node{
     private adjacentNodes : Array<AdjacentNodes>;
     private adjacentHex : Array<Hexagon>;
     private user : number;
     private type : number;
+    private harbourType : number;
 
     constructor(){
         this.adjacentNodes = new Array();
         this.adjacentHex = new Array();
         this.user = -1;
         this.type = -1;
+        this.harbourType = -1;
     }
     setAdjacentNode(node : Node){
         if(!this.adjacentNodes.some(element => element.node == node)) this.adjacentNodes.push({
@@ -59,16 +65,21 @@ class Node{
     setAdjacentHex(hexagon : Hexagon){
         if(!this.adjacentHex.includes(hexagon)) this.adjacentHex.push(hexagon);
     }
-
+    setHarbour(harbourType : number){
+        this.harbourType = harbourType;
+    }
 }
 
 class Hexagon{
-    nodes : Array<Node>;
-    hasRobber : boolean;
-    resource : number;
-    constructor(resource : number){
+    private nodes : Array<Node>;
+    private hasRobber : boolean;
+    private resource : number;
+    private tokenNumber : number;
+
+    constructor(resource : number, tokenNumber : number){
         this.nodes = new Array(HEXAGON.NUM_OF_NODES);
         this.resource = resource;
+        this.tokenNumber = tokenNumber;
     }
     setNode(node : Node, position : number) : void{
         this.nodes[position] = node;
@@ -80,49 +91,63 @@ class Hexagon{
 }
 
 export default class Board{
-    hexagons : Array<Array<Hexagon>>;
+    private hexagons : Array<Array<Hexagon>>;
     constructor(){
         let numOfResources = [1, 4, 4, 4, 3, 3];
+        let numOfRollNumber = [1, 2, 2, 2, 2, 0, 2, 2, 2, 2, 1];
         this.hexagons = new Array(MainBoard.NUM_OF_ROWS);
 
         for(let i = 0; i < MainBoard.NUM_OF_ROWS_INCREASING; i++){
             this.hexagons[i] = new Array(NUM_OF_HEXAGONS_PER_ROW[i]);
             for(let j = 0; j < NUM_OF_HEXAGONS_PER_ROW[i]; j++){
+
                 let resourceNumber = Math.floor(Math.random() * HEXAGON.NUM_OF_RESOURCES);
                 while(numOfResources[resourceNumber] <= 0){
                     resourceNumber = Math.floor(Math.random() * HEXAGON.NUM_OF_RESOURCES)
                 } 
                 numOfResources[resourceNumber]--;
-                let tempHexagon = new Hexagon(resourceNumber);
+
+                let tokenNumber;
+                if(resourceNumber == 0){
+                    tokenNumber = 0;
+                }
+                else{
+                    tokenNumber = Math.floor(Math.random() * 11);
+                    while(numOfRollNumber[tokenNumber] <= 0){
+                        tokenNumber = Math.floor(Math.random() * 11);
+                    }
+                    numOfRollNumber[tokenNumber]--;
+                    tokenNumber += 2;
+                }
+                this.hexagons[i][j] = new Hexagon(resourceNumber, tokenNumber);
 
                 if(i != 0){
                     if(j != 0){
-                        tempHexagon.setNode(this.hexagons[i - 1][j - 1].getNode(NODE.SOUTH_EAST), NODE.NORTH);
-                        tempHexagon.setNode(this.hexagons[i - 1][j - 1].getNode(NODE.SOUTH), NODE.NORTH_WEST);
+                        this.hexagons[i][j].setNode(this.hexagons[i - 1][j - 1].getNode(NODE.SOUTH_EAST), NODE.NORTH);
+                        this.hexagons[i][j].setNode(this.hexagons[i - 1][j - 1].getNode(NODE.SOUTH), NODE.NORTH_WEST);
                     }
                     if(j != NUM_OF_HEXAGONS_PER_ROW[i] - 1){
-                        if(tempHexagon.getNode(NODE.NORTH) == undefined) 
-                            tempHexagon.setNode(this.hexagons[i - 1][j].getNode(NODE.SOUTH_WEST), NODE.NORTH);
-                        tempHexagon.setNode(this.hexagons[i - 1][j].getNode(NODE.SOUTH), NODE.NORTH_EAST);
+                        if(this.hexagons[i][j].getNode(NODE.NORTH) == undefined) 
+                            this.hexagons[i][j].setNode(this.hexagons[i - 1][j].getNode(NODE.SOUTH_WEST), NODE.NORTH);
+                        this.hexagons[i][j].setNode(this.hexagons[i - 1][j].getNode(NODE.SOUTH), NODE.NORTH_EAST);
                     }
                 }
                 if(j != 0){
-                    if(tempHexagon.getNode(NODE.NORTH_WEST) == undefined) 
-                        tempHexagon.setNode(this.hexagons[i][j - 1].getNode(NODE.NORTH_EAST), NODE.NORTH_WEST);
-                    tempHexagon.setNode(this.hexagons[i][j - 1].getNode(NODE.SOUTH_EAST), NODE.SOUTH_WEST);
+                    if(this.hexagons[i][j].getNode(NODE.NORTH_WEST) == undefined) 
+                        this.hexagons[i][j].setNode(this.hexagons[i][j - 1].getNode(NODE.NORTH_EAST), NODE.NORTH_WEST);
+                    this.hexagons[i][j].setNode(this.hexagons[i][j - 1].getNode(NODE.SOUTH_EAST), NODE.SOUTH_WEST);
                 }
 
                 for(let k = 0; k < HEXAGON.NUM_OF_NODES; k++){
-                    if(tempHexagon.getNode(k) == undefined) tempHexagon.setNode(new Node, k);
+                    if(this.hexagons[i][j].getNode(k) == undefined) this.hexagons[i][j].setNode(new Node, k);
                 }
                 for(let k = 0; k < HEXAGON.NUM_OF_NODES; k++){
-                    tempHexagon.getNode(k).setAdjacentNode(tempHexagon.getNode((k + HEXAGON.NUM_OF_NODES - 1) % HEXAGON.NUM_OF_NODES));
-                    tempHexagon.getNode(k).setAdjacentNode(tempHexagon.getNode((k + 1) % HEXAGON.NUM_OF_NODES));
+                    this.hexagons[i][j].getNode(k).setAdjacentNode(this.hexagons[i][j].getNode((k + HEXAGON.NUM_OF_NODES - 1) % HEXAGON.NUM_OF_NODES));
+                    this.hexagons[i][j].getNode(k).setAdjacentNode(this.hexagons[i][j].getNode((k + 1) % HEXAGON.NUM_OF_NODES));
                 }
-
-                this.hexagons[i][j] = (tempHexagon);
             }
         }
+        
         for(let i = MainBoard.NUM_OF_ROWS_INCREASING; i < MainBoard.NUM_OF_ROWS; i++){
             this.hexagons[i] = new Array(NUM_OF_HEXAGONS_PER_ROW[i]);
             for(let j = 0; j < NUM_OF_HEXAGONS_PER_ROW[i]; j++){
@@ -131,23 +156,45 @@ export default class Board{
                     resourceNumber = Math.floor(Math.random() * HEXAGON.NUM_OF_RESOURCES)
                 } 
                 numOfResources[resourceNumber]--;
-                let tempHexagon = new Hexagon(resourceNumber);
 
-                tempHexagon.setNode(this.hexagons[i - 1][j].getNode(NODE.SOUTH), NODE.NORTH_WEST);
-                tempHexagon.setNode(this.hexagons[i - 1][j].getNode(NODE.SOUTH_EAST), NODE.NORTH);
-                tempHexagon.setNode(this.hexagons[i - 1][j + 1].getNode(NODE.SOUTH), NODE.NORTH_EAST);
-                if(j != 0) tempHexagon.setNode(this.hexagons[i][j - 1].getNode(NODE.SOUTH_EAST), NODE.SOUTH_WEST);
+                let tokenNumber;
+                if(resourceNumber == 0){
+                    tokenNumber = 0;
+                }
+                else{
+                    tokenNumber = Math.floor(Math.random() * 11);
+                    while(numOfRollNumber[tokenNumber] <= 0){
+                        tokenNumber = Math.floor(Math.random() * 11);
+                    }
+                    numOfRollNumber[tokenNumber]--;
+                    tokenNumber += 2;
+                }
+                this.hexagons[i][j] = new Hexagon(resourceNumber, tokenNumber);
+
+                this.hexagons[i][j].setNode(this.hexagons[i - 1][j].getNode(NODE.SOUTH), NODE.NORTH_WEST);
+                this.hexagons[i][j].setNode(this.hexagons[i - 1][j].getNode(NODE.SOUTH_EAST), NODE.NORTH);
+                this.hexagons[i][j].setNode(this.hexagons[i - 1][j + 1].getNode(NODE.SOUTH), NODE.NORTH_EAST);
+                if(j != 0) this.hexagons[i][j].setNode(this.hexagons[i][j - 1].getNode(NODE.SOUTH_EAST), NODE.SOUTH_WEST);
                 
                 for(let k = 0; k < HEXAGON.NUM_OF_NODES; k++){
-                    if(tempHexagon.getNode(k) == undefined) tempHexagon.setNode(new Node, k);
+                    if(this.hexagons[i][j].getNode(k) == undefined) this.hexagons[i][j].setNode(new Node, k);
                 }
                 for(let k = 0; k < HEXAGON.NUM_OF_NODES; k++){
-                    tempHexagon.getNode(k).setAdjacentNode(tempHexagon.getNode((k + HEXAGON.NUM_OF_NODES - 1) % HEXAGON.NUM_OF_NODES));
-                    tempHexagon.getNode(k).setAdjacentNode(tempHexagon.getNode((k + 1) % HEXAGON.NUM_OF_NODES));
+                    this.hexagons[i][j].getNode(k).setAdjacentNode(this.hexagons[i][j].getNode((k + HEXAGON.NUM_OF_NODES - 1) % HEXAGON.NUM_OF_NODES));
+                    this.hexagons[i][j].getNode(k).setAdjacentNode(this.hexagons[i][j].getNode((k + 1) % HEXAGON.NUM_OF_NODES));
                 }
-
-                this.hexagons[i][j] = (tempHexagon);
             }
         }
+
+        let numOfHarbours = [4, 1, 1, 1, 1, 1];
+        HARBOUR_LOC.forEach(element => {
+            let harbourType = Math.floor(Math.random() * 6);
+            while(numOfHarbours[harbourType] <= 0) {
+                harbourType = Math.floor(Math.random() * 6);
+            }
+            numOfHarbours[harbourType]--;
+            this.hexagons[element[0]][element[1]].getNode(element[2]).setHarbour(harbourType);
+            this.hexagons[element[0]][element[1]].getNode(element[3]).setHarbour(harbourType);
+        });
     }
 }
