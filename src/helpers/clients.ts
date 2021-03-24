ca9d02759e7d68d4dfe00847a8e89b86971c35a0
Gordon Lin <contact@gordonlin.ca>
2021-03-23 21:14:57 -0400

    Added if user joined a game

--- src/helpers/clients.ts
+++ src/helpers/clients.ts
@@ -1,27 +1,32 @@
 interface Client {
-    name: String,
-    guid: String,
-    connnection: any
+    name: string,
+    guid: string,
+    connnection: any,
+    joinedGame : boolean
 }
 
 export default class Clients {
-    clientMap: Map<String, Client>;
+    private clientMap: Map<string, Client>;
     constructor(){
         this.clientMap = new Map();
         this.saveClient = this.saveClient.bind(this);
         this.getClient = this.getClient.bind(this);
     }
-    getClient(guid : String) : Client{
+    getClient(guid : string) : Client{
         return this.clientMap.get(guid);
     }
-    saveClient(name : String, guid : String, connection : any) : void{
+    saveClient(name : string, guid : string, connection : any) : void{
         this.clientMap.set(guid, {
             name: name,
             guid: guid,
-            connnection: connection
+            connnection: connection,
+            joinedGame : false
         });
     }
-    removeClient(guid : String) : boolean {
+    removeClient(guid : string) : boolean {
         return this.clientMap.delete(guid);
+    }
+    updateClient(guid : string, key : string, value : string) : void{
+        this.clientMap.get(guid)[key] = value;
     }
 }
