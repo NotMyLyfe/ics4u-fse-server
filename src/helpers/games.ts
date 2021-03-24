dc33462f6b543e6b52489c639c341d97ea96395d
Gordon Lin <contact@gordonlin.ca>
2021-03-23 21:15:31 -0400

    Added constructor for game

--- src/helpers/games.ts
+++ src/helpers/games.ts
@@ -1,5 +1,33 @@
+import Board from './board';
+
+enum GameConst{
+    MAX_PLAYERS = 4,
+    MIN_PLAYERS = 3
+}
+
+interface gamePlayer {
+    name : string,
+    guid : string,
+    resources : Array<number>,
+    victoryPoints : number
+
+}
+
 export default class Games{
+    private users : Array<gamePlayer>;
+    private board : Board;
+    private gameStarted : boolean;
     constructor(){
-        
+        this.users = new Array();
+        this.board = new Board();
+        this.gameStarted = false;
     }
+    startGame() : boolean{
+        if(this.users.length < GameConst.MAX_PLAYERS) return false;
+        return this.gameStarted = true;
+    }
+    joinGame() : boolean {
+        if(this.users.length >= GameConst.MIN_PLAYERS) return false;
+        return true;
+    }   
 }
