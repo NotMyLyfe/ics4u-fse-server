de00f344d855b32151ac65858f1ba44f801d785f
Gordon Lin <contact@gordonlin.ca>
2021-03-24 23:05:10 -0400

    Included joined game, updated to include error callback

--- src/helpers/clients.ts
+++ src/helpers/clients.ts
@@ -1,16 +1,14 @@
-interface Client {
+export interface Client {
     name: string,
     guid: string,
     connnection: any,
-    joinedGame : boolean
+    joinedGame : string
 }
 
 export default class Clients {
     private clientMap: Map<string, Client>;
     constructor(){
         this.clientMap = new Map();
-        this.saveClient = this.saveClient.bind(this);
-        this.getClient = this.getClient.bind(this);
     }
     getClient(guid : string) : Client{
         return this.clientMap.get(guid);
@@ -20,13 +18,19 @@ export default class Clients {
             name: name,
             guid: guid,
             connnection: connection,
-            joinedGame : false
+            joinedGame : undefined
         });
     }
     removeClient(guid : string) : boolean {
         return this.clientMap.delete(guid);
     }
-    updateClient(guid : string, key : string, value : string) : void{
-        this.clientMap.get(guid)[key] = value;
+    updateClient(guid : string, key : string, value : string, errorCallback : (ws : any, message : string) => any, successCallback : (ws : any, additional : any) => any) : void{
+        try{
+            this.clientMap.get(guid)[key] = value;
+            successCallback(this.clientMap.get(guid).connnection, {});
+        }
+        catch(err){
+            errorCallback(this.clientMap.get(guid).connnection, err);
+        }
     }
 }
